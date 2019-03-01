c535c7bdbb2b1f675ebb528f62c5e45296e388af
wrogdenmoore <46682569+wrogdenmoore@users.noreply.github.com>
2019-03-01 02:49:21 -0500

    Initial Commit

---
+++ RoboAdvisor.py
@@ -0,0 +1,20 @@
+
+import requests
+import json
+
+print("requesting stock market data...")
+
+
+request_url = "https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=MSFT&interval=5min&apikey=demo"
+response = requests.get(request_url)
+# print(type(response)) #class 'requests.models.response
+# print(response.status_code)
+# print(response.text)
+
+parsed_response = json.loads
+
+breakpoint()
+
+
+
+quit()
